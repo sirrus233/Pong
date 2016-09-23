3eda8e74fed328b56d72c6897377fe1b3934b3b9
Bradley Sherman <sirrus233@gmail.com>
2016-09-23 16:45:51 -0700

    Added a title screen and refactored into a more OOP approach

--- pong.py
+++ pong.py
@@ -27,39 +27,40 @@ class Velocity:
         self.direction = 0
 
 class Ball(GameObject):
-    def __init__(self):
+    def __init__(self, batch):
         self.velocity = Velocity()
         self.acceleration = 0
         self.sprite = pyglet.sprite.Sprite(AssetManager.textures["BALL"], batch=batch)
     
-    def update(self, dt):
-        if self.collision(top_wall) or self.collision(bottom_wall):
-            self.velocity.direction = -self.velocity.direction
-
-        if self.collision(left_paddle) or self.collision(right_paddle):
-            self.velocity.direction = -self.velocity.direction + math.pi
-            self.velocity.speed += self.acceleration
-
+    def update(self):
         x = self.x + self.velocity.speed * math.cos(self.velocity.direction) 
         y = self.y + self.velocity.speed * math.sin(self.velocity.direction) 
         self.set_position(x, y)
 
 class Paddle(GameObject):
-    def __init__(self, up_key, down_key):
+    def __init__(self, batch, up_key, down_key):
         self.up_key = up_key
         self.down_key = down_key
         self.speed = 10
         self.sprite = pyglet.sprite.Sprite(AssetManager.textures["BAR"], batch=batch)
 
-    def update(self, dt):
+    def update(self, ball, top_wall, bottom_wall):
+        if self.collision(ball):
+            ball.velocity.direction = -ball.velocity.direction + math.pi
+            ball.velocity.speed += ball.acceleration
+
         if keys[self.up_key] and not self.collision(top_wall):
             self.set_position(self.x, self.y+self.speed)
         elif keys[self.down_key] and not self.collision(bottom_wall):
             self.set_position(self.x, self.y-self.speed)
             
 class Wall(GameObject):
-    def __init__(self):
+    def __init__(self, batch):
         self.sprite = pyglet.sprite.Sprite(AssetManager.textures["WALL"], batch=batch)
+
+    def update(self, ball):
+        if self.collision(ball):
+            ball.velocity.direction = -ball.velocity.direction
 
 class AssetManager:
     textures = dict()
@@ -77,23 +78,84 @@ class AssetManager:
         wall_img.height = 20
         AssetManager.textures["WALL"] = wall_img.get_texture()
 
+class Screen:
+    def __init__(self):
+        self.batch = pyglet.graphics.Batch()
+        window.push_handlers(self.on_draw)
+        pyglet.clock.schedule_interval(self.update, 0.01)
+
+    def on_draw(self):
+        window.clear()
+        self.batch.draw()
+    
+    def update(self, dt):
+        pass
+
+    def set_screen(self, screen):
+        window.pop_handlers() 
+        pyglet.clock.unschedule(self.update)
+        screen()
+
+class TitleScreen(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+
+        title_label = pyglet.text.Label(
+                'Pyglet Pong',
+                font_name='Times New Roman',
+                font_size=36,
+                x=window.width//2, y=window.height//2,
+                anchor_x='center', anchor_y='center',
+                batch=self.batch)
+
+        start_label = pyglet.text.Label(
+                'Press [ENTER] to start',
+                font_name='Times New Roman',
+                font_size=16,
+                x=window.width//2, y=window.height//2 - 60,
+                anchor_x='center', anchor_y='center',
+                batch=self.batch)
+
+    def update(self, dt):
+        if keys[key.ENTER]:
+            self.set_screen(GameScreen)
+        
+
+class GameScreen(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+
+        # Game object construction 
+        self.ball = Ball(self.batch)
+        self.left_paddle = Paddle(self.batch, key.W, key.S)
+        self.right_paddle = Paddle(self.batch, key.UP, key.DOWN)
+        self.top_wall = Wall(self.batch)
+        self.bottom_wall = Wall(self.batch)
+
+        # Game object initialization
+        self.ball.set_position(window.width//2, window.height//2)
+        self.ball.velocity.speed = 5
+        self.ball.velocity.direction = math.pi/4
+        self.ball.acceleration = 1
+    
+        paddle_offset = 20
+        self.left_paddle.set_position(paddle_offset, window.height//2)
+        self.right_paddle.set_position(window.width-paddle_offset-self.right_paddle.sprite.width , window.height//2)
+    
+        self.top_wall.set_position(0, window.height-self.top_wall.sprite.height)
+        self.bottom_wall.set_position(0, 0)
+
+    def update(self, dt):
+        self.ball.update()
+        self.left_paddle.update(self.ball, self.top_wall, self.bottom_wall)
+        self.right_paddle.update(self.ball, self.top_wall, self.bottom_wall)
+        self.top_wall.update(self.ball)
+        self.bottom_wall.update(self.ball)
+    
 # Window and game setup
 window = pyglet.window.Window(width=1024, height=768)
 keys = key.KeyStateHandler()
-batch = pyglet.graphics.Batch()
 AssetManager.load()
-
-# Game object initialization
-ball = Ball()
-left_paddle = Paddle(key.W, key.S)
-right_paddle = Paddle(key.UP, key.DOWN)
-top_wall = Wall()
-bottom_wall = Wall()
-
-@window.event
-def on_draw():
-    window.clear()
-    batch.draw()
 
 @window.event
 def on_key_press(symbol, modifiers):
@@ -103,22 +165,10 @@ def on_key_press(symbol, modifiers):
 window.push_handlers(keys)
 
 def main():
-    ball.set_position(window.width//2, window.height//2)
-    ball.velocity.speed = 5
-    ball.velocity.direction = math.pi/4
-    ball.acceleration = 1
-    
-    paddle_offset = 20
-    left_paddle.set_position(paddle_offset, window.height//2)
-    right_paddle.set_position(window.width-paddle_offset-right_paddle.sprite.width , window.height//2)
-
-    top_wall.set_position(0, window.height-top_wall.sprite.height)
-    bottom_wall.set_position(0, 0)
-
-    pyglet.clock.schedule_interval(ball.update, 0.01)
-    pyglet.clock.schedule_interval(left_paddle.update, 0.01)
-    pyglet.clock.schedule_interval(right_paddle.update, 0.01)
+    TitleScreen()
     pyglet.app.run()
 
 if __name__ == "__main__":
     main()
+
+
